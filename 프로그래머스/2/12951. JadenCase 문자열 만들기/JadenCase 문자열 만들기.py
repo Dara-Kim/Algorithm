8ad074ea2ee2dfadb7f919d77b0b77bf0122da5a
Daeun Kim <59412608+Dara-Kim@users.noreply.github.com>
2024-05-05 14:10:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,22 +1,48 @@
 # 1번째 풀이(런타임 에러)
 def solution(s):
-    string = list(s.split(" "))
+    string = list(s.split())
     answer = []
     
     for alp in string:
         alp = alp[0].upper() + alp[1:].lower()
         answer.append(alp)
+        
     return ' '.join(answer)
 
-# 2번째 풀이
+# 2번째 풀이(그냥 틀림)
+'''
+1) s.split()
+2) s.split(" ")
+
+둘 모두 공백 문자를 기준으로 문자를 분리하는 것은 맞음
+1)의 경우 연속된 공백 문자는 무시됨
+2)의 경우 공백 문자 사이 추가 공백도 모두 포함하여 문자열 분리
+
+즉, 'a____a'가
+['a', 'a'] 또는 ['a', '_', '_', '_', 'a']로 분리되는 것의 차이
+
+'''
 def solution(s):
-    string = list(s.split(" "))
+    string = list(s.split()) # -> s.split(" "). "문제 조건: 공백문자가 연속해서 나올 수 있습니다."
     answer = []
     
     for a in string:
-        answer.append(a.capitalize())
+        answer.append(a.capitalize()) # 반례: 'aaaAA'
+        
     return ' '.join(answer)
 
-# 숏코딩
+# 3번째 풀이(런타임 에러)
 def solution(s):
-    return ' '.join([a.capitalize() for a in list(s.split(" "))])
+    answer = ''
+    words = s.split(" ")    
+    for i, word in enumerate(words):
+        if word and not word.isdigit(): 
+            f_letter = word[0].upper()
+            rest_word = word[1:].lower()
+            words[i] = f_letter + rest_word
+    return ' '.join(words)
+
+
+# # 숏코딩
+# def solution(s):
+#     return ' '.join([a[0].upper()+a[1:].lower() for a in list(s.split(" "))])
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.00 ms
+메모리: 10.1 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 05일 13:47:02
+2024년 05월 05일 14:10:55
 
 ### 문제 설명
 
